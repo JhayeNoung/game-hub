7ecb4a0fd32ddb733ef8648421b27b2390a2979f
James <james.noung@gmail.com>
2024-11-14 21:09:00 +0700

    building search bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,19 @@
 import { HStack, Image} from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
 import ToggleDarkMode from './ToggleDarkMode'
+import SearchInput from './SearchInput'
 
-function NavBar() {
+interface Props{
+  onSearch: (searchValue: string) => void;
+}
+
+function NavBar({onSearch}: Props) {
   return (
     <>
         {/* 'justifyContent' document is under Components > Layout > Flex */}
         <HStack justifyContent='space-between' padding='10px'>
             <Image src={logo} boxSize="60px"/>
+            <SearchInput submitHandler={(event)=>onSearch(event.searchName)}/>
             <ToggleDarkMode/>
         </HStack>
     </>
