f0318a2259f20f51effc8378cfe9ad9aed2d9e76
James <james.noung@gmail.com>
2025-06-13 17:31:43 +0700

    final

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,23 @@
-import { Box, HStack, Image} from '@chakra-ui/react'
+import { Box, HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
 import ToggleDarkMode from './ToggleDarkMode'
 import SearchInput from './SearchInput'
 
-interface Props{
+interface Props {
   onSearch: (searchValue: string) => void;
 }
 
-function NavBar({onSearch}: Props) {
+function NavBar({ onSearch }: Props) {
   return (
     <>
-    {/* 'justifyContent' document is under Components > Layout > Flex */}
-    <HStack justifyContent='space-between' padding='10px'>
-      <Image src={logo} boxSize="50px"/>
-      <Box width="full" >
-        <SearchInput submitHandler={(event)=>onSearch(event.searchName)}/>
-      </Box>
-      <ToggleDarkMode/>
-    </HStack>
+      {/* 'justifyContent' document is under Components > Layout > Flex */}
+      <HStack justifyContent='space-between' padding='10px'>
+        <Image src={logo} boxSize="50px" />
+        <Box width="full" >
+          <SearchInput submitHandler={(event) => onSearch(event.searchName)} />
+        </Box>
+        <ToggleDarkMode />
+      </HStack>
     </>
   )
 }
